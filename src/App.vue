f81c81995907db6b09f5751d8170561d336dc02f
wansoo jang <jwansoo5197@gmail.com>
2026-02-01 07:26:12 +0900

    third change

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-const hello = ref('hello')
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <h1>You did it!</h1>
-  <p>{{ hello }}</p>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
 </template>
 
 <style scoped></style>
